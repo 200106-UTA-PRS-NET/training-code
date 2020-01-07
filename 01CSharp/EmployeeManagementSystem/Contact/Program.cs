b916b953fd326c5ac4549af549185dd3559f7a22
Pushpinder Kaur <kaurrevature@outlook.com>
2020-01-07 17:19:24 -0600

    sln, properties, access modifiers

---
+++ 01CSharp/EmployeeManagementSystem/Contact/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+// import the contactLib
+using ContactLib;
+
+namespace ContactUi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************Revature EMS*************");
+            // Initialize the class or instantiate the object
+            Contact contact = new Contact();
+            // String Concat - bad practise
+            //Console.WriteLine("Default Employee Name "+ contact.firstName+" "+contact.lastName);
+            contact.FirstName = "Nicky";
+            // use String Builder class instead or string interpolation
+            StringBuilder emp = new StringBuilder();// creates mutable string which cane be appended in the same memory location
+            emp.Append("Default Employee Name ").Append(contact.FirstName).Append(" ").Append(contact.LastName);
+            Console.WriteLine(emp);
+            string empDetails=contact.GetEmployee("Fred", "Belotte", 40, "9876543211", "fred@revature.com", 123);
+            Console.WriteLine(empDetails);
+        }
+    }
+}
---
+++ 01CSharp/EmployeeManagementSystem/ContactLib/Contact.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ContactLib
+{
+    public class Contact
+    {
+        //variables - string, int, byte, double, float, bool, char, DateTime, long
+       private string firstName;
+       private string lastName;
+       private int age;
+       private string phone;
+       private string email;
+       private long id;
+        // autonomous property
+        public bool IsFullTime { get; set; }
+
+        // properties - smart fields which are used to provide public access to a private variable
+        public string FirstName
+        {
+            get// allows you to read the private value
+            {
+                return firstName;
+            }
+            // comment - select the code ctrl + K+C
+            // uncomment - select code and Ctrl + K + U
+            set// allows you to write that private variable
+            {
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get// allows you to read the private value
+            {
+                return lastName;
+            }
+            set// allows you to write that private variable
+            {
+                lastName = value;
+            }
+        }
+        public string GetEmployee(string firstName, string lastName, int age, string phone, string email, long id)
+        {
+            // this points to current member of the current class
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.phone = phone;
+            this.email = email;
+            this.id = id;
+
+            return $"Name - {firstName} {lastName} \n Age - {age} \n Phone - {phone} \n Email - {email} \n Emp Id- {id}";
+        }
+        //Constructor - ctor + <tab> + <tab>
+        public Contact()
+        {
+            firstName = "Nick";
+            lastName = "Escalona";
+            phone = "9876543211";
+        }
+    }
+}
