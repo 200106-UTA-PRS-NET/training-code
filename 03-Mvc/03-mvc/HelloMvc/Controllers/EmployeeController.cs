619c65517415c90cc6fb36c8100b374add11c86d
Pushpinder Kaur <kaurrevature@outlook.com>
2020-01-23 15:49:57 -0600

    HTML Helper and Views

--- 03-Mvc/03-mvc/HelloMvc/Controllers/EmployeeController.cs
+++ 03-Mvc/03-mvc/HelloMvc/Controllers/EmployeeController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using HelloMvc.Models;
+using System;
+
 namespace HelloMvc.Controllers{
     public class EmployeeController:Controller{
-        List<Employee> employees=new List<Employee>(){
+       static List<Employee> employees=new List<Employee>(){
             new Employee(){
                 Id=1,
                 FirstName="Fred",
@@ -15,8 +17,28 @@ namespace HelloMvc.Controllers{
                 LastName="Coley"
             }
         };
+        [HttpGet]
         public ActionResult GetEmployee(){
-            return View(employees);
+            var date=Display();
+            //ViewData["Employee"]=employees; // this needs type casting in View
+            //ViewBag.Employee=employees; // this does not needs any type casting in Views 
+            //return View();
+            return View(employees);// passing Model to the view
         }
+        [NonAction]// this will not be treated as an action method at all
+        public string Display(){
+            return DateTime.Now.ToShortDateString();
+        }
+
+        public ViewResult AddEmployee(){
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee){
+            
+           employees.Add(employee);
+           return RedirectToAction("GetEmployee");
+        }
+
     }
 }
