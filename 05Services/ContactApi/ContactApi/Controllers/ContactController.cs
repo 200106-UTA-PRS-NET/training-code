861506364217b3dfe7ba5b9bafff2d5bf12fb4bf
Pushpinder <pushpinder.kaur@revature.com>
2020-02-06 15:22:24 -0600

    Added a new contact in feature -1

--- 05Services/ContactApi/ContactApi/Controllers/ContactController.cs
+++ 05Services/ContactApi/ContactApi/Controllers/ContactController.cs
@@ -12,7 +12,8 @@ namespace ContactApi.Controllers
         static List<Contact> contacts = new List<Contact>() { 
             new Contact(){Id=1, FirstName="Princy",LastName="Kalsi",contactType=ContactType.mobile,Phone="9876543210"},
             new Contact(){Id=2,FirstName="Annei",LastName="Kaur", contactType=ContactType.personal,Phone="1234567890"},
-            new Contact(){Id=3,FirstName="Jamey",LastName="Wilson", contactType=ContactType.work,Phone="7890543222"}
+            new Contact(){Id=3,FirstName="Jamey",LastName="Wilson", contactType=ContactType.work,Phone="7890543222"},
+            new Contact(){Id=4,FirstName="Fred",LastName="Belotte", contactType=ContactType.work,Phone="0987654323"}
         };
 
         public IEnumerable<Contact> Get()
