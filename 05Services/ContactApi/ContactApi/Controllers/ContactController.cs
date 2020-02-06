1630ab7b61d1fee6027e8665044b915d56f9bd1e
Pushpinder <pushpinder.kaur@revature.com>
2020-02-06 15:19:25 -0600

    intro to web api

---
+++ 05Services/ContactApi/ContactApi/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using ContactApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]   
+    public class ContactController : ControllerBase
+    {
+        static List<Contact> contacts = new List<Contact>() { 
+            new Contact(){Id=1, FirstName="Princy",LastName="Kalsi",contactType=ContactType.mobile,Phone="9876543210"},
+            new Contact(){Id=2,FirstName="Annei",LastName="Kaur", contactType=ContactType.personal,Phone="1234567890"},
+            new Contact(){Id=3,FirstName="Jamey",LastName="Wilson", contactType=ContactType.work,Phone="7890543222"}
+        };
+
+        public IEnumerable<Contact> Get()
+        {
+            return contacts;
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody]Contact contact)
+        {
+            contacts.Add(contact);
+            return Ok($"{ contact.Id} is added");
+        }
+
+    }
+}
---
+++ 05Services/ContactApi/ContactApi/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactApi.Models
+{
+    public enum ContactType
+    {
+        work,
+        personal,
+        mobile,
+        home
+    }
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string  LastName { get; set; }
+        public string Phone { get; set; }
+        public ContactType contactType { get; set; }
+    }
+}
