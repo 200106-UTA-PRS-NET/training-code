ea74877a870796581a5ff5a852849fbd3aeb1165
Pushpinder <pushpinder.kaur@revature.com>
2020-02-13 11:28:35 -0600

    forgot to add notes for TP-WP-NPM-Demo

--- 06ClientSide-JS,TS,NG/TP-WP-NPM/src/card-service.ts
+++ 06ClientSide-JS,TS,NG/TP-WP-NPM/src/card-service.ts
@@ -3,7 +3,7 @@ import Card from "./card";
 import DrawCardResponse from "./draw-card-response";
 
 export default class CardService {
-    deckId: string | null = null;
+    deckId: any = null;
     urlPrefix = 'https://deckofcardsapi.com/api'
 
     newDeck(): Promise<string> {
